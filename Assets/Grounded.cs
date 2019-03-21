e69b63bebbe1107a9c1d27f9667f03c7726fe129
Leandro Peres <leandroperes@protonmail.com>
2019-03-21 19:31:46 -0300

    Initial commit

---
+++ Assets/Grounded.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    public bool onGround = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onGround |= other.name == "Ground";
+        print(other.name);
+    }
+
+}
